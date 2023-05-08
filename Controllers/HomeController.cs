9d3e7cdacbf8b37b331138aecc800fa72883c16e
SIMÓN SUKEN ROSENBERG <47296430@ort>
2023-05-08 10:31:06 -0300

    Controllers finalizados

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,17 +6,28 @@ public class EstudianteController : Controller
 {
     public IActionResult Index()
     {
-        
+        ViewBag.Paquetes = ortWorld.Paquetes;
         return View();
     }
     public IActionResult SelectPaquete()
     {
-        
+        ViewBag.ListaDestinos = ortWorld.ListaDestinos;
+        ViewBag.ListaAereos = ortWorld.ListaAereos;
+        ViewBag.ListaHoteles = ortWorld.ListaHoteles;
+        ViewBag.ListaExcursiones = ortWorld.ListaExcursiones;
         return View();
     }
     public IActionResult GuardarPaquete(int Destino, int Hotel, int Aereo, int Excursion)
     {
-        
-        return View();
+        if (Destino > 0 && Hotel > 0 && Aereo > 0 && Excursion > 0)
+        {
+            Paquete paq = new Paquete(ortWorld.ListaHoteles[Hotel], ortWorld.ListaAereos[Aereo], ortWorld.ListaExcursiones[Excursion]);
+            ortWorld.ingresarPaquete(ortWorld.ListaDestinos[Destino], paq);
+            return View("index.cshtml");
+        }
+        else
+        {
+            return View("selectPaquete.cshtml");
+        }
     }
 }
